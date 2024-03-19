6c584b1484ee87b9c3f32fc55f3b1fb3a0d56d6e
LordRock02 <rogergt02@gmail.com>
2024-03-19 02:12:54 +0000

    v2

--- analizador lexico javascript/analizadorLexico.py
+++ analizador lexico javascript/analizadorLexico.py
@@ -1,10 +1,24 @@
 import re
 
 # Listas para los diferentes tipos de tokens en Javascript
-operators = ['+', '-', '*', '=', '/', '%', '++', '--', '==', '!=', '>', '<', '>=', '<=', '&&', '||', '!', '&', '|', '^', '~', '<<', '>>', '>>>', '+=', '-=', '*=', '/=', '%=', '<<=', '>>=', '>>>=', '&=', '|=', '^=']
-literals = ['true', 'false', 'NULL', 'undefined', 'NaN', 'Infinity', '""', "''", "``"]
-delimiters = ['(', ')', '{', '}', '[', ']', '"', "'", "`", ',', ';', ':', '.', '?', '=>']
-keyWords = ['if', 'else', 'else if', 'switch', 'case', 'default', 'while', 'do', 'for', 'break', 'continue', 'return', 'function', 'var', 'let', 'const', 'new', 'delete', 'in', 'instanceof', 'typeof', 'void', 'this', 'super', 'class', 'extends', 'import', 'export', 'async', 'await', 'try', 'catch', 'finally', 'throw', 'with', 'debugger', 'arguments', 'yield']
+operators = [
+    '+', '-', '*', '=', '/', '%', '++', '--', '==', '!=', '>', '<', '>=', '<=',
+    '&&', '||', '!', '&', '|', '^', '~', '<<', '>>', '>>>', '+=', '-=', '*=',
+    '/=', '%=', '<<=', '>>=', '>>>=', '&=', '|=', '^='
+]
+literals = [
+    'true', 'false', 'NULL', 'undefined', 'NaN', 'Infinity', '""', "''", "``"
+]
+delimiters = [
+    '(', ')', '{', '}', '[', ']', '"', "'", "`", ',', ';', ':', '.', '?', '=>'
+]
+keyWords = [
+    'if', 'else', 'else if', 'switch', 'case', 'default', 'while', 'do', 'for',
+    'break', 'continue', 'return', 'function', 'var', 'let', 'const', 'new',
+    'delete', 'in', 'instanceof', 'typeof', 'void', 'this', 'super', 'class',
+    'extends', 'import', 'export', 'async', 'await', 'try', 'catch', 'finally',
+    'throw', 'with', 'debugger', 'arguments', 'yield'
+]
 comments = ['//', '/*', '*/']
 
 # Expresion regular para los identificadores
@@ -18,22 +32,29 @@ stringRegex = r'"[^"]+"|\'[^\']+\'|`[^`]+`'
 
 tokensRegex = r'"[^"]+"|\'[^\']+\'|`[^`]+`|\S+|"(?=\s)|\'(?=\s)|`(?=\s)'
 
+
 def analizarLexico(code):
 
-		tokens = re.findall(tokensRegex, code)
-		print(tokens)
+	tokens = re.findall(tokensRegex, code)
+	print(tokens)
 
-		for c in tokens:
-				if any(c in lista for lista in [operators, literals, delimiters, keyWords, comments]):
-						continue
-				elif re.search(identifierRegex, c):
-						continue
-				elif re.search(stringRegex, c):
-						continue
-				elif re.search(numberRegex, c):
-						continue
-				return False
-		return True 
+	for c in tokens:
+		if any(c in lista
+		       for lista in [operators, literals, delimiters, keyWords, comments]):
+			print(f'Token Especial: {c}')
+			continue
+		elif re.search(identifierRegex, c):
+			print(f'Identificador: {c}')
+			continue
+		elif re.search(stringRegex, c):
+			print(f'String: {c}')
+			continue
+		elif re.search(numberRegex, c):
+			print(f'Numero: {c}')
+			continue
+		return False
+	return True
+
 
 '''print('code : ')
 code = input()
